c6d4197035a3ab73691146c0248c48114aba8452
alireza <sdamoosavi@gmail.com>
2020-09-03 22:37:11 +0430

    modifying recEngine part

--- server/main/app/Utils/recUtils/RecEngine.py
+++ server/main/app/Utils/recUtils/RecEngine.py
@@ -1,13 +1,18 @@
 from app.Utils.recUtils.builder import *
 
+
 def recBooks(book, k):
     idx = getBookId(book=book)
     if idx:
-        
-        bookIndx = getSimilarIndx(idx, k)
-        rec_books = getListResult(bookIndx)
+
+        listScores = list(enumerate(cosine_sim_corpus[idx]))
+        sim_scores = sorted(listScores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1 : k + 1]
+
+        book_indices = [i[0] for i in sim_scores]
+        rec_books = getListResult(book_indices)
 
         return rec_books, getBookWithid(idx)
-        
+
     else:
         return None, [book, None]
--- server/main/app/Utils/recUtils/builder.py
+++ server/main/app/Utils/recUtils/builder.py
@@ -1,35 +1,46 @@
+from pickle import load
+from pandas import read_csv
+
+
+with open("app/Utils/recUtils/sim_matrix.pickle", "rb") as handle:
+    cosine_sim_corpus = load(handle)
+
 
 def load_data():
-    from pandas import read_csv
-    return read_csv('app/Utils/recUtils/goodreads_data.csv',index_col=0)
+
+    return read_csv("app/Utils/recUtils/goodreads_data.csv", index_col=0)
+
+
 books_df = load_data()
 
 
 def look_for_book(df, name):
     return df[df.title.str.contains(name.lower(), na=False)]
 
+
 def getBookId(book):
     book_df = look_for_book(books_df, name=book)
-    
+
     if book_df.empty:
         return None
     else:
         return book_df.index.values[0]
 
+
 def getBookWithid(x):
-    d= [books_df.title.iloc[x], books_df.authors.iloc[x]]
+    d = [books_df.title.iloc[x], books_df.authors.iloc[x]]
     return d
+
 
 def getDictResult(bookInx):
     res = []
     for i in range(len(bookInx)):
         x = bookInx[i]
-        d = {i+1: {'book' : books_df.title.iloc[x], 'authors' : books_df.authors.iloc[x]}} 
+        d = {
+            i + 1: {"book": books_df.title.iloc[x], "authors": books_df.authors.iloc[x]}
+        }
         res.append(d)
     return res
-
-def getSimilarIndx(id, k):
-    return books_df.sample(k).index.to_list()
 
 
 def getListResult(bookInx):
